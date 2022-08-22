30e853422d603da4ad6b46ab2c8c10fdc8b3f37e
Joshua Khooba <joshuak@gmail.com>
2022-08-22 11:36:23 -0400

    commit on master

---
+++ storybook2.py
@@ -0,0 +1,9 @@
+#Joshua Khooba
+#COP 2500.0001
+#Day 1 Assignemnt 0
+#Aug 22, 2022
+
+print("Story", "\t\t\tType", "\t\t\tDate")
+print("Naruto Shippuden", "\tTV show", "\t\t2007")
+print("Attack on Titan", "\tTV show", "\t\t2013")
+print("House of Cards", "\t\tTV show", "\t\t2013")
